3b858c18d7f9f7e27ce97f16b499a0f846fb42b8
abogazee515-droid <abogazee515@gmail.com>
2026-03-31 17:19:52 +0700

    Add simple if function

--- conditional_program.cpp
+++ conditional_program.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+// Simple If Example
+void SimpleIfExample()
+{
+    int x = 5;
+
+    if (x == 5)
+    {
+        x = x + 1;
+        cout << "Value of x has been corrected to: " << x << endl;
+    }
+
+    cout << "Generated number = " << x << endl;
+}
